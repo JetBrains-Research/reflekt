d4e22438eaaf8b757d836df1aa9d95e599296135
Anastasiia.Birillo <nbirillo@mail.ru>
2020-09-21 20:41:55 +0300

    Change example according to new architecture

--- examples/src/main/kotlin/io/reflekt/example/Main.kt
+++ examples/src/main/kotlin/io/reflekt/example/Main.kt
@@ -3,10 +3,13 @@ package io.reflekt.example
 import io.reflekt.Reflekt
 
 fun main() {
-    val objects = Reflekt.objects().withSubType<AInterface>().toList()
+    val objects = Reflekt.objects().withSubType<AInterface>("io.reflekt.example.AInterface").toList()
     println(objects.joinToString { it.description() })
 
 
-    val classes = Reflekt.classes().withSubType<BInterface>().toList()
+    val classes = Reflekt.classes().withSubType<BInterface>("io.reflekt.example.BInterface").toList()
     println(classes.joinToString { it.toString() })
 }
+
+// io.reflekt.Reflekt.Objects.withSubType
+// io.reflekt.Reflekt.Classes.withSubType
