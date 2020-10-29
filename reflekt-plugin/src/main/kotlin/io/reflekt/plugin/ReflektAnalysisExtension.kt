b404eb08fc9f91d5267da0c5cc37d82a3570ab42
Anastasiia.Birillo <nbirillo@mail.ru>
2020-10-29 20:15:39 +0300

    Use analysisCompleted instead of doAnalysis

--- reflekt-plugin/src/main/kotlin/io/reflekt/plugin/ReflektAnalysisExtension.kt
+++ reflekt-plugin/src/main/kotlin/io/reflekt/plugin/ReflektAnalysisExtension.kt
@@ -5,8 +5,6 @@ import io.reflekt.plugin.utils.Util.log
 import org.jetbrains.kotlin.analyzer.AnalysisResult
 import org.jetbrains.kotlin.cli.common.messages.MessageCollector
 import org.jetbrains.kotlin.com.intellij.openapi.project.Project
-import org.jetbrains.kotlin.container.ComponentProvider
-import org.jetbrains.kotlin.context.ProjectContext
 import org.jetbrains.kotlin.descriptors.ModuleDescriptor
 import org.jetbrains.kotlin.psi.KtFile
 import org.jetbrains.kotlin.resolve.BindingTrace
@@ -15,15 +13,9 @@ import org.jetbrains.kotlin.resolve.jvm.extensions.AnalysisHandlerExtension
 class ReflektAnalysisExtension(private val filesToIntrospect: Set<KtFile>,
                                private val messageCollector: MessageCollector? = null) : AnalysisHandlerExtension {
 
-    override fun doAnalysis(project: Project,
-                            module: ModuleDescriptor,
-                            projectContext: ProjectContext,
-                            files: Collection<KtFile>,
-                            bindingTrace: BindingTrace,
-                            componentProvider: ComponentProvider): AnalysisResult? {
+    override fun analysisCompleted(project: Project, module: ModuleDescriptor, bindingTrace: BindingTrace, files: Collection<KtFile>): AnalysisResult? {
         messageCollector?.log("ReflektAnalysisExtension is starting...")
         messageCollector?.log("FILES: ${files.joinToString(separator = ", ") { it.name }};")
-        // TODO: we have null fqNames :(
         val analyzer = ReflektAnalyzer(files.toSet().union(filesToIntrospect), bindingTrace.bindingContext)
         val invokes = analyzer.invokes()
         messageCollector?.log("INVOKES: $invokes;")
@@ -31,7 +23,7 @@ class ReflektAnalysisExtension(private val filesToIntrospect: Set<KtFile>,
         messageCollector?.log("USES: $uses;")
         // TODO: add uses into bindingContext???
 
-        return super.doAnalysis(project, module, projectContext, files, bindingTrace, componentProvider)
+        return super.analysisCompleted(project, module, bindingTrace, files)
     }
 
 }
