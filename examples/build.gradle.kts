218983999a707ad08ba41d28e187cc3bd2597aca
Anastasiia.Birillo <nbirillo@mail.ru>
2021-08-10 15:21:56 +0300

    Change version

--- examples/build.gradle.kts
+++ examples/build.gradle.kts
@@ -26,7 +26,7 @@ allprojects {
     }
 
     dependencies {
-        implementation("io.reflekt", "reflekt-dsl", "0.2.0")
+        implementation("io.reflekt", "reflekt-dsl", "0.1.0")
         implementation("com.github.gumtreediff", "core", "2.1.2")
     }
 
