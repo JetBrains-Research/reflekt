9bf902c852cb518bfe3558e3c0e7f474872dab36
Anastasiia.Birillo <nbirillo@mail.ru>
2021-09-08 20:21:45 +0300

    Delete incorrect test

--- reflekt-plugin/src/test/resources/io/reflekt/plugin/code/generation/data/functions3_test/project/test/Main.kt
+++
@@ -1,8 +0,0 @@
-package io.reflekt.codegen.test
-
-import io.reflekt.Reflekt
-
-fun main() {
-    val functions = Reflekt.functions().withAnnotations<() -> Unit>(FirstAnnotationTest::class)
-    val functions1 = Reflekt.functions().withAnnotations<(Int) -> Unit>(FirstAnnotationTest::class)
-}
