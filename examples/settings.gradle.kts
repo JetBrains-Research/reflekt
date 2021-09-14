93b3cfaf4c5d54a129212c9741a1bfb1caa050af
Anastasiia.Birillo <nbirillo@mail.ru>
2021-09-14 21:36:50 +0300

    Use local maven

--- examples/settings.gradle.kts
+++ examples/settings.gradle.kts
@@ -4,19 +4,19 @@ include(":first-module")
 include(":second-module")
 
 pluginManagement {
-    resolutionStrategy {
-        this.eachPlugin {
-
-            if (requested.id.id == "io.reflekt") {
-                useModule("io.reflekt:gradle-plugin:${this.requested.version}")
-            }
-        }
-    }
+//    resolutionStrategy {
+//        this.eachPlugin {
+//
+//            if (requested.id.id == "io.reflekt") {
+//                useModule("io.reflekt:gradle-plugin:${this.requested.version}")
+//            }
+//        }
+//    }
 
     repositories {
         gradlePluginPortal()
-//        mavenLocal()
+        mavenLocal()
         //add the dependency to Reflekt Maven repository
-        maven(url = uri("https://packages.jetbrains.team/maven/p/reflekt/reflekt"))
+//        maven(url = uri("https://packages.jetbrains.team/maven/p/reflekt/reflekt"))
     }
 }
