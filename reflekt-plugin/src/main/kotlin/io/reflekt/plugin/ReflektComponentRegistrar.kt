665f7d92bef63c13ca36f1b3b6feffde0ed41661
Anastasiia.Birillo <nbirillo@mail.ru>
2021-10-15 18:47:10 +0300

    Delete absolute path

--- reflekt-plugin/src/main/kotlin/io/reflekt/plugin/ReflektComponentRegistrar.kt
+++ reflekt-plugin/src/main/kotlin/io/reflekt/plugin/ReflektComponentRegistrar.kt
@@ -19,7 +19,7 @@ import java.io.File
 @AutoService(ComponentRegistrar::class)
 class ReflektComponentRegistrar(private val hasConfiguration: Boolean = true) : ComponentRegistrar {
     // The path will be: pathToKotlin/daemon/reflekt-log.log
-    private val logFilePath = "/Users/Anastasiia.Birillo/IdeaProjects/reflekt/reflekt-log.log"
+    private val logFilePath = "reflekt-log.log"
 
     override fun registerProjectComponents(
         project: MockProject,
