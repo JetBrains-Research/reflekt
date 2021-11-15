4e9adee1220a6bdc4b5d717f759551dadfa683c2
Anastasiia.Birillo <nbirillo@mail.ru>
2021-11-15 20:02:49 +0300

    Code refactoring code a bit

--- reflekt-plugin/src/main/kotlin/io/reflekt/plugin/analysis/models/ReflektModels.kt
+++ reflekt-plugin/src/main/kotlin/io/reflekt/plugin/analysis/models/ReflektModels.kt
@@ -14,9 +14,24 @@ import org.jetbrains.kotlin.resolve.BindingContext
 import org.jetbrains.kotlin.types.KotlinType
 import org.jetbrains.kotlin.types.Variance
 
+typealias ClassOrObjectInvokes = MutableSet<SupertypesToAnnotations>
+typealias FunctionInvokes = MutableSet<SignatureToAnnotations>
+typealias SerializableFunctionInvokes = MutableSet<SerializableSignatureToAnnotations>
+
+typealias TypeUses<K, V> = MutableMap<K, MutableList<V>>
+typealias ClassOrObjectUses = TypeUses<SupertypesToAnnotations, KtClassOrObject>
+typealias FunctionUses = TypeUses<SignatureToAnnotations, KtNamedFunction>
+
+typealias IrClassOrObjectUses = TypeUses<SupertypesToAnnotations, String>
+typealias IrFunctionUses = TypeUses<SignatureToAnnotations, IrFunctionInfo>
+
+typealias BaseInvokesProcessors = Set<BaseInvokesProcessor<*>>
+
 /**
  * If the function [withAnnotations] is called without supertypes then [supertypes] is setOf(Any::class::qualifiedName)
  * If the function [withSupertypes] is called without annotations then [annotations] is empty
+ * @property supertypes
+ * @property annotations
  */
 @Serializable
 data class SupertypesToAnnotations(
@@ -24,6 +39,12 @@ data class SupertypesToAnnotations(
     val annotations: Set<String> = emptySet()
 )
 
+/**
+ * @property fqName
+ * @property arguments
+ * @property returnType
+ * @property receiverType
+ */
 @Serializable
 data class SerializableKotlinType(
     val fqName: String,
@@ -32,6 +53,11 @@ data class SerializableKotlinType(
     val receiverType: SerializableKotlinType?
 )
 
+/**
+ * @property fqName
+ * @property isStarProjection
+ * @property projectionKind
+ */
 @Serializable
 data class SerializableTypeProjection(
     val fqName: String,
@@ -39,21 +65,30 @@ data class SerializableTypeProjection(
     val projectionKind: Variance,
 )
 
+/**
+ * @property signature
+ * @property annotations
+ */
 @Serializable
 data class SerializableSignatureToAnnotations(
     var signature: SerializableKotlinType?, // kotlin.FunctionN< ... >
     val annotations: Set<String> = emptySet(),
 )
 
+/**
+ * @property signature
+ * @property annotations
+ */
 data class SignatureToAnnotations(
     var signature: KotlinType?, // kotlin.FunctionN< ... >
     val annotations: Set<String> = emptySet(),
 )
 
-typealias ClassOrObjectInvokes = MutableSet<SupertypesToAnnotations>
-typealias FunctionInvokes = MutableSet<SignatureToAnnotations>
-typealias SerializableFunctionInvokes = MutableSet<SerializableSignatureToAnnotations>
-
+/**
+ * @property objects
+ * @property classes
+ * @property functions
+ */
 @Serializable
 data class SerializableReflektInvokes(
     val objects: HashMap<FileId, ClassOrObjectInvokes> = HashMap(),
@@ -61,6 +96,10 @@ data class SerializableReflektInvokes(
     val functions: HashMap<FileId, SerializableFunctionInvokes> = HashMap()
 )
 
+/**
+ * @property invokes
+ * @property packages
+ */
 @Serializable
 data class SerializableReflektInvokesWithPackages(
     val invokes: SerializableReflektInvokes,
@@ -84,6 +123,10 @@ data class SerializableReflektInvokesWithPackages(
         )
 }
 
+/**
+ * @property invokes
+ * @property packages
+ */
 data class ReflektInvokesWithPackages(
     val invokes: ReflektInvokes,
     val packages: Set<String>
@@ -95,13 +138,18 @@ data class ReflektInvokesWithPackages(
         )
 }
 
+/**
+ * @property objects
+ * @property classes
+ * @property functions
+ */
 data class ReflektInvokes(
     val objects: HashMap<FileId, ClassOrObjectInvokes> = HashMap(),
     val classes: HashMap<FileId, ClassOrObjectInvokes> = HashMap(),
     val functions: HashMap<FileId, FunctionInvokes> = HashMap()
 ) {
     companion object {
-        fun createByProcessors(processors: Set<BaseInvokesProcessor<*>>) = ReflektInvokes(
+        fun createByProcessors(processors: BaseInvokesProcessors) = ReflektInvokes(
             objects = processors.mapNotNull { it as? ObjectInvokesProcessor }.first().fileToInvokes,
             classes = processors.mapNotNull { it as? ClassInvokesProcessor }.first().fileToInvokes,
             functions = processors.mapNotNull { it as? FunctionInvokesProcessor }.first().fileToInvokes
@@ -134,30 +182,25 @@ data class ReflektInvokes(
         this.also { second.forEach { (k, v) -> this.getOrPut(k) { v } } }
 }
 
-typealias TypeUses<K, V> = MutableMap<K, MutableList<V>>
-typealias ClassOrObjectUses = TypeUses<SupertypesToAnnotations, KtClassOrObject>
-typealias FunctionUses = TypeUses<SignatureToAnnotations, KtNamedFunction>
-
-/* Stores enough information to generate function reference IR */
+/**
+ * Stores enough information to generate function reference IR
+ *
+ * @property fqName
+ * @property receiverFqName
+ * @property isObjectReceiver
+ */
 data class IrFunctionInfo(
     val fqName: String,
     val receiverFqName: String?,
-    val isObjectReceiver: Boolean
+    val isObjectReceiver: Boolean,
 )
-
-typealias IrClassOrObjectUses = TypeUses<SupertypesToAnnotations, String>
-typealias IrFunctionUses = TypeUses<SignatureToAnnotations, IrFunctionInfo>
-
-fun ClassOrObjectUses.toSupertypesToFqNamesMap(): Map<Set<String>, List<String>> {
-    return this.map { it.key.supertypes to it.value.mapNotNull { it.fqName?.toString() } }.toMap()
-}
-
-fun FunctionUses.toAnnotationsToFunction(): Map<Set<String>, List<KtNamedFunction>> {
-    return this.map { it.key.annotations to it.value }.toMap()
-}
 
 /**
  * Store a set of qualified names that match the conditions for each item from [ReflektInvokes]
+ *
+ * @property objects
+ * @property classes
+ * @property functions
  */
 data class ReflektUses(
     val objects: HashMap<FileId, ClassOrObjectUses> = HashMap(),
@@ -173,16 +216,11 @@ data class ReflektUses(
     }
 }
 
-fun <T, V : KtElement> HashMap<FileId, TypeUses<T, V>>.flatten(): TypeUses<T, V> {
-    val uses: TypeUses<T, V> = HashMap()
-    this.forEach { (_, typeUses) ->
-        typeUses.forEach { (k, v) ->
-            uses.getOrPut(k) { mutableListOf() }.addAll(v)
-        }
-    }
-    return uses
-}
-
+/**
+ * @property objects
+ * @property classes
+ * @property functions
+ */
 data class IrReflektUses(
     val objects: IrClassOrObjectUses = HashMap(),
     val classes: IrClassOrObjectUses = HashMap(),
@@ -203,4 +241,17 @@ data class IrReflektUses(
     )
 
     private fun <K, V> TypeUses<K, V>.merge(second: TypeUses<K, V>): TypeUses<K, V> = this.also { second.forEach { (k, v) -> this.getOrPut(k) { v } } }
+}
+
+fun ClassOrObjectUses.toSupertypesToFqNamesMap() = this.map { it.key.supertypes to it.value.mapNotNull { it.fqName?.toString() } }.toMap()
+
+@Suppress("IDENTIFIER_LENGTH", "TYPE_ALIAS")
+fun <T, V : KtElement> HashMap<FileId, TypeUses<T, V>>.flatten(): TypeUses<T, V> {
+    val uses: TypeUses<T, V> = HashMap()
+    this.forEach { (_, typeUses) ->
+        typeUses.forEach { (k, v) ->
+            uses.getOrPut(k) { mutableListOf() }.addAll(v)
+        }
+    }
+    return uses
 }
