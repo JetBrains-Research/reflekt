4e993c81ff83847db120cbdcc05fef921f4a7692
Adam <897017+aSemy@users.noreply.github.com>
2022-03-18 23:49:08 +0100

    rm zip4j (replaced with Gradle zipTree)

--- reflekt-core/src/main/kotlin/org/jetbrains/reflekt/util/file/FileUtils.kt
+++ reflekt-core/src/main/kotlin/org/jetbrains/reflekt/util/file/FileUtils.kt
@@ -4,36 +4,42 @@
 
 package org.jetbrains.reflekt.util.file
 
-import net.lingala.zip4j.ZipFile
 import java.io.File
 
-/**
- * Unzip `this` file and get all low-level files in subdirectories
- *
- * @return list with corresponding files
- */
-fun File.extractAllFiles(): List<File> = this.unZipFile().getAllNestedFiles()
+///**
+// * Unzip `this` file and get all low-level files in subdirectories
+// *
+// * @return list with corresponding files
+// */
+//fun File.extractAllFiles(): List<File> = this.unZipFile().getAllNestedFiles()
 
 /**
  * Get all low-level files in subdirectories of `this` rootPath
  *
- * @param files
  * @param ignoredDirectories
  * @return list with corresponding files
  */
 fun String.getAllNestedFiles(
-    files: MutableList<File> = ArrayList(),
+//    files: MutableList<File> = ArrayList(),
     ignoredDirectories: Set<String> = emptySet(),
 ): List<File> {
-    val root = File(this)
-    root.listFiles()?.forEach {
-        if (it.isFile) {
-            files.add(it)
-        } else if (it.isDirectory && it.name !in ignoredDirectories) {
-            it.absolutePath.getAllNestedFiles(files)
-        }
-    }
-    return files
+
+    return File(this)
+        .walkTopDown()
+        .onEnter { it.name !in ignoredDirectories }
+        .filter { it.isFile }
+        .onEach { println("found a file: $it") }
+        .toList()
+
+//    val root = File(this)
+//    root.listFiles()?.forEach {
+//        if (it.isFile) {
+//            files.add(it)
+//        } else if (it.isDirectory && it.name !in ignoredDirectories) {
+//            it.absolutePath.getAllNestedFiles(files)
+//        }
+//    }
+//    return files
 }
 
 /**
@@ -41,11 +47,14 @@ fun String.getAllNestedFiles(
  *
  * @return list with corresponding directories
  */
-fun String.getNestedDirectories() = File(this).listFiles()?.filter { it.isDirectory } ?: emptyList()
-
-private fun File.unZipFile(destinationPath: String? = null): String {
-    val zipFile = ZipFile(this.path)
-    val outputPath = destinationPath ?: this.parent
-    zipFile.extractAll(outputPath)
-    return outputPath
-}
+fun String.getNestedDirectories() =
+    File(this)
+        .walkTopDown().filter { it.isDirectory }.toList()
+//        .listFiles()?.filter { it.isDirectory } ?: emptyList()
+//
+//private fun File.unZipFile(destinationPath: String? = null): String {
+//    val zipFile = ZipFile(this.path)
+//    val outputPath = destinationPath ?: this.parent
+//    zipFile.extractAll(outputPath)
+//    return outputPath
+//}
