0133fd07489c247e8f6b462e995ccdd067288227
Anastasiia Birillo <nbirillo@mail.ru>
2022-07-09 10:24:03 +0200

    Add DUMP_REFLEKT_IMPL directive (#130)

--- reflekt-plugin/src/test/kotlin/org/jetbrains/reflekt/plugin/compiler/handlers/ReflektImplDumpHandler.kt
+++ reflekt-plugin/src/test/kotlin/org/jetbrains/reflekt/plugin/compiler/handlers/ReflektImplDumpHandler.kt
@@ -6,6 +6,8 @@ import org.jetbrains.kotlin.test.model.TestModule
 import org.jetbrains.kotlin.test.services.TestServices
 import org.jetbrains.kotlin.test.services.moduleStructure
 import org.jetbrains.kotlin.test.services.temporaryDirectoryManager
+import org.jetbrains.kotlin.test.utils.MultiModuleInfoDumper
+import org.jetbrains.reflekt.plugin.compiler.directives.ReflektPluginCallDirectives.DUMP_REFLEKT_IMPL
 import org.jetbrains.reflekt.plugin.compiler.providers.reflekt.ReflektPluginWithLibraryProvider
 import java.io.File
 
@@ -14,12 +16,10 @@ class ReflektImplDumpHandler(testServices: TestServices) : AbstractIrHandler(tes
         const val DUMP_EXTENSION = "expected.kt"
     }
 
+    private val dumper = MultiModuleInfoDumper()
+
     override fun processAfterAllModules(someAssertionWasFailed: Boolean) {
-        val tmpDirectory = testServices.temporaryDirectoryManager.getOrCreateTempDirectory(ReflektPluginWithLibraryProvider.TMP_DIRECTORY_NAME)
-        val actualFile = tmpDirectory.walkTopDown().find { it.name == "ReflektImpl.kt" } ?: error("Can not find ReflektImpl generated file")
-        // TODO: think about comparing ReflektImpl files (different orders of found entities)
-        println(actualFile.readText())
-        assertions.assertEqualsToFile(getExpectedFile(), actualFile.readText(), message = { "Content is not equal" })
+        assertions.assertEqualsToFile(getExpectedFile(), dumper.generateResultingDump(), message = { "Content is not equal" })
     }
 
     private fun getExpectedFile(): File {
@@ -27,7 +27,12 @@ class ReflektImplDumpHandler(testServices: TestServices) : AbstractIrHandler(tes
         return originalTestFolder.listFiles()?.find { it.name.endsWith(DUMP_EXTENSION) } ?: error("Can not find ReflektImpl expected file")
     }
 
-    @Suppress("EmptyFunctionBlock")
     override fun processModule(module: TestModule, info: IrBackendInput) {
+        if (DUMP_REFLEKT_IMPL !in module.directives) return
+        val builder = dumper.builderForModule(module)
+        val tmpDirectory = testServices.temporaryDirectoryManager.getOrCreateTempDirectory(ReflektPluginWithLibraryProvider.TMP_DIRECTORY_NAME)
+        val actualFile = tmpDirectory.walkTopDown().find { it.name == "ReflektImpl.kt" } ?: error("Can not find ReflektImpl generated file")
+        // TODO: think about comparing ReflektImpl files (different orders of found entities)
+        builder.append(actualFile.readText())
     }
 }
--- reflekt-plugin/src/test/kotlin/org/jetbrains/reflekt/plugin/compiler/runners/general/AbstractReflektWithLibraryTest.kt
+++ reflekt-plugin/src/test/kotlin/org/jetbrains/reflekt/plugin/compiler/runners/general/AbstractReflektWithLibraryTest.kt
@@ -3,6 +3,7 @@ package org.jetbrains.reflekt.plugin.compiler.runners.general
 import org.jetbrains.kotlin.test.builders.TestConfigurationBuilder
 import org.jetbrains.kotlin.test.builders.configureIrHandlersStep
 import org.jetbrains.kotlin.test.runners.codegen.commonHandlersForCodegenTest
+import org.jetbrains.reflekt.plugin.compiler.directives.ReflektPluginCallDirectives.DUMP_REFLEKT_IMPL
 import org.jetbrains.reflekt.plugin.compiler.handlers.ReflektImplDumpHandler
 import org.jetbrains.reflekt.plugin.compiler.providers.reflekt.ReflektPluginWithLibraryProvider
 import org.jetbrains.reflekt.plugin.compiler.runners.AbstractReflektRuntimeClassPathTest
@@ -16,6 +17,10 @@ open class AbstractReflektWithLibraryTest : AbstractReflektRuntimeClassPathTest(
 
 fun TestConfigurationBuilder.reflektWithLibrary() {
     reflektRuntimeClassPath()
+
+    defaultDirectives {
+        +DUMP_REFLEKT_IMPL
+    }
 
     commonHandlersForCodegenTest()
 
